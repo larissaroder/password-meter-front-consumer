504a4f76628acb52f06bf48ba825cfd1e0103419
Larissa Bonifacio Roder <larissaroder@gmail.com>
2016-10-31 07:15:23 -0300

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,6 @@
 var app = angular.module("passwordValidation",['passwordCtrl']);
 
+//Interpolação do Angular alterada para não haver conflito com a interpolação do template usado (handlesbar)
 app.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('{[{');
     $interpolateProvider.endSymbol('}]}');
